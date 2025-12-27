343a5e656061de104e4f3955c5a2de07789b1af4
Michael Farkas <mail@farkmi.dev>
2025-12-27 20:53:19 +0100

    feat: remove generated swagger.yml and update API generation script

--- generate_api.sh
+++ generate_api.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+SWAGGER_URL=${1:-"http://localhost:8081/swagger.yml"}
+
+echo "Fetching swagger.yml from $SWAGGER_URL..."
+
 mkdir -p packages/spinsnitch_api
+
 npx openapi-generator-cli generate \
-    -i api/swagger.yml \
+    -i "$SWAGGER_URL" \
     -g dart \
     -o packages/spinsnitch_api \
     --additional-properties=pubName=spinsnitch_api,pubDescription=SpinSnitchApiClient
